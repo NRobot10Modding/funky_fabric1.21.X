73d8dcec5bebe91c0e2c00de9b07ddcc9dcc7ea6
NRobot10Modding <nathanr11.ingham@outlook.com>
2024-09-13 15:47:04 +0100

    files

---
+++ src/main/java/net/nrobot10/funky_fabric/FunkyFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nrobot10.funky_fabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FunkyFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
