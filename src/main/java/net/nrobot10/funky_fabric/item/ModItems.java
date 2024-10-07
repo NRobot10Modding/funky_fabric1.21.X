297ab66f38dfb6354eb6893561bb337a50e9f437
NRobot10Modding <nathanr11.ingham@outlook.com>
2024-10-07 19:18:29 +0100

    files

--- src/main/java/net/nrobot10/funky_fabric/item/ModItems.java
+++ src/main/java/net/nrobot10/funky_fabric/item/ModItems.java
@@ -4,6 +4,9 @@ package net.nrobot10.funky_fabric.item;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
 
@@ -11,6 +14,6 @@ public class ModItems {
         entries.add(Items.STONE);
     }
     private static Item registerItem(String name, Item item) {
-
+        return Registry.register(Registries.ITEM, Identifier.tryParse())
     }
 }
