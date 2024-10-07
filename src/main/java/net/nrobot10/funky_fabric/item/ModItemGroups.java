47863834cd7fc4a013bb7d8e7b850fe6d52059ec
NRobot10Modding <nathanr11.ingham@outlook.com>
2024-10-07 19:28:38 +0100

    files

--- src/main/java/net/nrobot10/funky_fabric/item/ModItemGroups.java
+++ src/main/java/net/nrobot10/funky_fabric/item/ModItemGroups.java
@@ -1,6 +1,10 @@
 package net.nrobot10.funky_fabric.item;
 
+import net.nrobot10.funky_fabric.FunkyFabric;
+
 public class ModItemGroups {
 
-    public static void registerItemGroups()
+    public static void registerItemGroups() {
+        FunkyFabric.LOGGER.info("Registering Mod Item Groups for " + FunkyFabric.MOD_ID);
+    }
 }
