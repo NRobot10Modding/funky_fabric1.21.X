2ebb0a24b9b0467325d8c135fb57fd6d5c0cfbcb
NRobot10Modding <nathanr11.ingham@outlook.com>
2024-10-07 19:26:35 +0100

    files

---
+++ src/main/java/net/nrobot10/funky_fabric/item/ModItemGroups.java
@@ -0,0 +1,6 @@
+package net.nrobot10.funky_fabric.item;
+
+public class ModItemGroups {
+
+    public static void registerItemGroups()
+}
--- src/main/java/net/nrobot10/funky_fabric/item/ModItems.java
+++ src/main/java/net/nrobot10/funky_fabric/item/ModItems.java
@@ -2,18 +2,25 @@ package net.nrobot10.funky_fabric.item;
 
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.nrobot10.funky_fabric.FunkyFabric;
 
 public class ModItems {
 
-    private static void registerModItems(FabricItemGroupEntries entries) {
+    private static void addItemsToFirstItemGroup(FabricItemGroupEntries entries) {
         entries.add(Items.STONE);
     }
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.tryParse())
+        return Registry.register(Registries.ITEM, Identifier.tryParse(FunkyFabric.MOD_ID + ":" + name), item);
+    }
+
+    public static void registerModItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(ModItems::addItemsToFirstItemGroup);
     }
 }
